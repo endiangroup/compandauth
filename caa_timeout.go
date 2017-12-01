7ece3e55dd5f7514fea8290cf22a998cfc0430b0
Adrian Duke <adrian.duke@gmail.com>
2017-12-01 16:18:49 +0000

    Update TimeoutCAA to confrom to new CAA interface

--- caa_timeout.go
+++ caa_timeout.go
@@ -8,32 +8,36 @@ import (
 
 type TimeoutCAA int64
 
+func NewTimeout() *TimeoutCAA {
+	return new(TimeoutCAA)
+}
+
 // Locks CAA to prevent validation of session CAA's.
-func (caa TimeoutCAA) Lock() CAA {
-	return -caa.abs()
+func (caa *TimeoutCAA) Lock() {
+	*caa = -caa.abs()
 }
 
 // Unlocks CAA to allow validation of session CAA's.
-func (caa TimeoutCAA) Unlock() CAA {
-	return caa.abs()
+func (caa *TimeoutCAA) Unlock() {
+	*caa = caa.abs()
 }
 
 func (caa TimeoutCAA) IsLocked() bool {
 	return caa < 0
 }
 
-// Indicates if an incoming session CAA is considered valid. incomingCAA should
+// Indicates if an session session CAA is considered valid. sessionCAA should
 // be the CAA value retrieved from a session token (e.g. JWT). durationSecs represents
 // number of seconds you would like to consider a session valid for.
-func (caa TimeoutCAA) IsValid(incomingTimestamp int64, durationSecs int64) bool {
-	incomingTimestamp = abs(incomingTimestamp)
+func (caa TimeoutCAA) IsValid(s SessionCAA, durationSecs int64) bool {
+	sessionTimestamp := abs(int64(s))
 	durationSecs = abs(durationSecs)
 	expiryTimestamp := int64(caa)
 
 	return !caa.IsLocked() &&
 		caa.HasIssued() &&
-		incomingTimestamp >= expiryTimestamp &&
-		(incomingTimestamp+durationSecs) >= clock.Now().Unix()
+		sessionTimestamp >= expiryTimestamp &&
+		(sessionTimestamp+durationSecs) >= clock.Now().Unix()
 }
 
 // Utility function to convert time.Duration into int64 seconds
@@ -45,28 +49,25 @@ func ToSeconds(d time.Duration) int64 {
 // timestamp in seconds). If CAA hasn't ever issued expiryTimstamp is ignored and the
 // CAA is returned as is. If CAA is locked it will perform necessary
 // conversions on expiryTimstamp. Set to now to invalid all previously issued sessions.
-func (caa TimeoutCAA) Revoke(expiryTimestamp int64) CAA {
+func (caa *TimeoutCAA) Revoke(expiryTimestamp int64) {
 	if !caa.HasIssued() {
-		return caa
+		return
 	}
 
-	if caa.IsLocked() {
-		return TimeoutCAA(-abs(expiryTimestamp))
-	}
-
-	return TimeoutCAA(abs(expiryTimestamp))
+	caa.set(expiryTimestamp)
 }
 
 // Issues the next CAA value to use in a distributed session and the CAA. If
 // locked it will still return the next valid session CAA value. CAA is only
 // set on first issue.
-func (caa TimeoutCAA) Issue() (int64, CAA) {
+func (caa *TimeoutCAA) Issue() SessionCAA {
 	now := clock.Now().Unix()
+
 	if !caa.HasIssued() {
-		return now, TimeoutCAA(now)
+		caa.set(now)
 	}
 
-	return now, caa
+	return SessionCAA(now)
 }
 
 // Indicates if the CAA has issued at least once, regardless if it has been
@@ -79,4 +80,12 @@ func (caa TimeoutCAA) abs() TimeoutCAA {
 	return TimeoutCAA(abs(int64(caa)))
 }
 
-var _ = CAA(TimeoutCAA(0))
+func (caa *TimeoutCAA) set(i int64) {
+	if caa.IsLocked() {
+		*caa = TimeoutCAA(-abs(i))
+	} else {
+		*caa = TimeoutCAA(abs(i))
+	}
+}
+
+var _ = CAA(NewCounter())
