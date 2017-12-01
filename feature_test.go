8096c0a4862988156cc07f159b092c32b78e6e12
Adrian Duke <adrian.duke@gmail.com>
2017-12-01 17:37:13 +0000

    Added new Counter feature test for Revoke

--- feature_test.go
+++ feature_test.go
@@ -8,13 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func setCounterCAA(i int64) *compandauth.CounterCAA {
-	caa := compandauth.NewCounter()
-	*caa = compandauth.CounterCAA(i)
-
-	return caa
-}
-
 type Entity struct {
 	Delta int64
 	CAA   compandauth.CAA
@@ -22,6 +15,13 @@ type Entity struct {
 
 type Session struct {
 	CAA compandauth.SessionCAA
+}
+
+func setCounterCAA(i int64) *compandauth.CounterCAA {
+	caa := compandauth.NewCounter()
+	*caa = compandauth.CounterCAA(i)
+
+	return caa
 }
 
 func Test_Counter_ItConsidersUnissuedCAAsAsAlwaysInvalid(t *testing.T) {
@@ -120,4 +120,84 @@ func Test_Counter_OnlyTheLastDeltaSessionsAreConsideredValid(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_Counter_ItRevokesTheLastNSessions(t *testing.T) {
+	tests := []struct {
+		Delta            int64
+		NumberOfSessions int64
+		RevokeN          int64
+	}{
+		{Delta: 0, NumberOfSessions: 0, RevokeN: 0},
+		{Delta: 0, NumberOfSessions: 0, RevokeN: 1},
+		{Delta: 0, NumberOfSessions: 1, RevokeN: 1},
+		{Delta: 1, NumberOfSessions: 1, RevokeN: 0},
+		//{Delta: 1, NumberOfSessions: 0, RevokeN: 0},
+		{Delta: 1, NumberOfSessions: 0, RevokeN: 1},
+		{Delta: 1, NumberOfSessions: 1, RevokeN: 1},
+		{Delta: 1, NumberOfSessions: 2, RevokeN: 2},
+		{Delta: 10, NumberOfSessions: 10, RevokeN: 1},
+		{Delta: 10, NumberOfSessions: 10, RevokeN: 2},
+		{Delta: 10, NumberOfSessions: 10, RevokeN: 3},
+		{Delta: 10, NumberOfSessions: 10, RevokeN: 4},
+		{Delta: 10, NumberOfSessions: 10, RevokeN: 5},
+		{Delta: 10, NumberOfSessions: 10, RevokeN: 6},
+		{Delta: 10, NumberOfSessions: 10, RevokeN: 7},
+		{Delta: 10, NumberOfSessions: 10, RevokeN: 8},
+		{Delta: 10, NumberOfSessions: 10, RevokeN: 9},
+		{Delta: 10, NumberOfSessions: 10, RevokeN: 10},
+		{Delta: 10, NumberOfSessions: 10, RevokeN: 11},
+		{Delta: 5, NumberOfSessions: 100, RevokeN: 50},
+		{Delta: 5, NumberOfSessions: 100, RevokeN: 99},
+		{Delta: 5, NumberOfSessions: 100, RevokeN: 150},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%+v", test), func(t *testing.T) {
+			entity := Entity{Delta: test.Delta, CAA: compandauth.NewCounter()}
+			sessions := []Session{}
+
+			for i := int64(0); i < test.NumberOfSessions; i++ {
+				newSession := Session{}
+				newSession.CAA = entity.CAA.Issue()
+
+				sessions = append(sessions, newSession)
+			}
+
+			entity.CAA.Revoke(test.RevokeN)
+
+			lastRevokedSessionOffset := min(
+				test.NumberOfSessions-entity.Delta+test.RevokeN,
+				test.NumberOfSessions,
+			)
+			expectedNumberOfValidSessions := max(
+				entity.Delta-test.RevokeN,
+				0,
+			)
+			assert.Len(t, sessions[lastRevokedSessionOffset:], int(expectedNumberOfValidSessions))
+
+			for _, session := range sessions[lastRevokedSessionOffset:] {
+				assert.True(t, entity.CAA.IsValid(session.CAA, entity.Delta))
+			}
+
+			for _, session := range sessions[:lastRevokedSessionOffset] {
+				assert.False(t, entity.CAA.IsValid(session.CAA, entity.Delta))
+			}
+
+		})
+	}
+}
+
+func min(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+	return b
 }
